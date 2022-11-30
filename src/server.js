b4e23c7ecd1622c6c476b3d07611b1bd774927ef
hyewonyu <gva05165@naver.com>
2022-11-30 23:28:47 +0900

    chrom <-> firefox 양방향 통신

--- src/server.js
+++ src/server.js
@@ -13,16 +13,24 @@ const wss = new WebSocket.Server({ server });
 function onSocketClose() {
   console.log("Disconnected from the Browser ❌");
 }
-
 const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", onSocketClose);
-  socket.on("message", (message) => {
-    console.log(message.toString("utf-8"));
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        console.log(message.toString("utf-8"));
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
 });
 
