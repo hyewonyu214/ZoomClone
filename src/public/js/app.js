65767b24dce41a945b1d6c6fba8ff2179ee12b29
hyewonyu <gva05165@naver.com>
2022-12-01 23:56:39 +0900

    socket통신

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,9 +1,7 @@
 const socket = io();
-
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
-
 room.hidden = true;
 
 let roomName;
@@ -11,8 +9,18 @@ let roomName;
 function addMessage(message) {
   const ul = room.querySelector("ul");
   const li = document.createElement("li");
-  li.innerText = messages;
+  li.innerText = message;
   ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
 }
 
 function showRoom() {
@@ -20,6 +28,8 @@ function showRoom() {
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -35,3 +45,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
   addMessage("someone joined!");
 });
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
