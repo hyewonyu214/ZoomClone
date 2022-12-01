2b0cc23dae5f42138821bea7812e276150daa6b0
hyewonyu <gva05165@naver.com>
2022-12-01 23:37:34 +0900

    1개의 채팅룸에 여러사람 대화가능 SOCKET통신

--- src/server.js
+++ src/server.js
@@ -14,7 +14,14 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
 });
 
 //const wss = new WebSocket.Server({server});
